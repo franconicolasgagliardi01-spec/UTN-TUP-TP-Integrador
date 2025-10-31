8fd28a9cf03e51698bc106edb4fa69085d42eac4
Ramiro Quiroga <quirogarami11@gmail.com>
2025-10-31 14:40:26 -0300

    Actualizo función filtrar y subo función ordenar

--- Gestion de Paises/Filtrar.py
+++ Gestion de Paises/Filtrar.py
@@ -1,44 +1,47 @@
 #Filtrado por continentes
 def continente(paises):
- 
- agrupados = {} #Creo el diccionario agrupados{}
+    agrupados = {}  #Creo el diccionario agrupados{}
 
- #Recorro el diccionario paises para extraer país y continente, y así rellenar el diccionario creado previamente
- for nombre, datos in paises.items():
-     continente = datos[3]
-     if continente not in agrupados:
-         agrupados[continente] = [] #Se agrega el continente en caso de que no aparezca previamente
-     agrupados[continente].append(nombre)
+    #Recorro el diccionario paises para extraer país y continente, y así rellenar el diccionario creado previamente
+    for nombre, datos in paises.items():
+        continente = datos[3]
+        if continente not in agrupados:
+            agrupados[continente] = []  #Se agrega el continente en caso de que no aparezca previamente
+        agrupados[continente].append(nombre)
+        
+    print("-----------------------------------------------------")
+    print("Filtrando países por continente:")
+    #Se muestran por pantalla los paises agrupados por continente
+    for continente in sorted(agrupados):
+        print(f"{continente}:")
+        for pais in sorted(agrupados[continente]):
+            print(f"  - {pais}")
 
- print("Filtrando países por continente:\n")
- #Se muestran por pantalla los paises agrupados por continente
- for continente in sorted(agrupados):
-     print(f"\n{continente}:")
-     for pais in sorted(agrupados[continente]):
-         print(f"  - {pais}")
 
- #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Filtrado por rango de población
 def rango_poblacion(paises):
-    
     while True:
+        print("-----------------------------------------------------")
         print("Ingrese rango de población a buscar (en millones de personas):")
 
         try:
             #Al ingresar los rangos en millones, multiplico por 1000000 para llegar al número deseado
-            rangoMin = int(input("Desde: ")) * 1000000 
+            rangoMin = int(input("Desde: ")) * 1000000
             rangoMax = int(input("Hasta: ")) * 1000000
         except ValueError:
+            print("-----------------------------------------------------")
             print("Error. Ingrese un número válido.")
             continue
 
         #Validación para verificar que el rango sea correcto
         if rangoMin < 0 or rangoMax < 0 or rangoMin >= rangoMax:
-            print("\nError. Ingrese valores válidos (el rango máximo debe ser mayor que el mínimo)\n")
+            print("-----------------------------------------------------")
+            print("Error. Ingrese valores válidos (el rango máximo debe ser mayor que el mínimo)\n")
             continue
 
-        paises_en_rango = [] #Creo la lista paises_en_rango[]
+        paises_en_rango = []  # Creo la lista paises_en_rango[]
 
         #Relleno la lista creada previamente
         for nombre, datos in paises.items():
@@ -48,48 +51,50 @@ def rango_poblacion(paises):
 
         #Imprimo por pantalla los paises dentro del rango ingresado
         if paises_en_rango:
-             print(f"\nPaíses dentro del rango de {rangoMin:,} y {rangoMax:,} habitantes:\n")
-             for pais in paises_en_rango:
-                 print(f"- {pais}: {paises[pais][1]:,} habitantes")
-
-        #Si la lista paises_en_rango está vacía (por lo que no se encuentraron países en tal rango) se imprime el siguiente mensaje
-        else:  
+            print("-----------------------------------------------------")
+            print(f"Países dentro del rango de {rangoMin:,} y {rangoMax:,} habitantes:\n")
+            for pais in paises_en_rango:
+                print(f"- {pais}: {paises[pais][1]:,} habitantes")
+        else:
+            #Si la lista paises_en_rango está vacía (por lo que no se encuentraron países en tal rango)
+            print("-----------------------------------------------------")
             print(f"No se encontraron países entre {rangoMin:,} y {rangoMax:,} habitantes")
 
         #Validación de salida
         while True:
-         salida = input("¿Desea filtrar paises por rango de población nuevamente? (s/n): ").lower()
-         if salida.lower() == "s":
-             break
-         elif salida.lower() == "n":
-             return
-         else:
+            salida = input("\n¿Desea ordenar paises por rango de población nuevamente?\n - Si desea continuar presione ¨s¨\n - Si desea salir presione ¨n¨\n\n").lower()
+            if salida == "s":
+                break
+            elif salida == "n":
+                return
+            else:
                 print("Error. Ingrese:\n - 's' para continuar\n - 'n' para salir")
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Filtrado por rango de superficie
 def rango_superficie(paises):
-
- while True:
+    while True:
+        print("-----------------------------------------------------")
         print("Ingrese rango de superficie a buscar:")
 
         try:
-           
             #Solicito los valores del rango
             rangoMin = int(input("Desde (km²): "))
             rangoMax = int(input("Hasta (km²): "))
- 
         except ValueError:
+            print("-----------------------------------------------------")
             print("Error. Ingrese un número válido.")
             continue
 
         #Validación para verificar que el rango sea correcto
         if rangoMin < 0 or rangoMax < 0 or rangoMin >= rangoMax:
-            print("\nError. Ingrese valores válidos (el rango máximo debe ser mayor que el mínimo)\n")
+            print("-----------------------------------------------------")
+            print("Error. Ingrese valores válidos (el rango máximo debe ser mayor que el mínimo)\n")
             continue
 
-        paises_en_rango = [] #Creo la lista paises_en_rango[]
+        paises_en_rango = []  #Creo la lista paises_en_rango[]
 
         #Relleno la lista creada previamente
         for nombre, datos in paises.items():
@@ -99,20 +104,20 @@ def rango_superficie(paises):
 
         #Imprimo por pantalla los paises dentro del rango ingresado
         if paises_en_rango:
-             print(f"\nPaíses dentro del rango de {rangoMin:,} y {rangoMax:,} km²:\n")
-             for pais in paises_en_rango:
-                 print(f"- {pais}: {paises[pais][2]:,} km²")
-
-        #Si la lista paises_en_rango está vacía (por lo que no se encuentraron países en tal rango) se imprime el siguiente mensaje
-        else:  
+            print("-----------------------------------------------------")
+            print(f"Países dentro del rango de {rangoMin:,} y {rangoMax:,} km²:\n")
+            for pais in paises_en_rango:
+                print(f"- {pais}: {paises[pais][2]:,} km²")
+        else:
+            #Si la lista paises_en_rango está vacía (por lo que no se encuentraron países en tal rango)
             print(f"No se encontraron países entre {rangoMin:,} y {rangoMax:,} km² de superficie")
 
-        #Validación de salida
+         #Validación de salida
         while True:
-         salida = input("¿Desea filtrar paises por rango de superficie nuevamente? (s/n): ").lower()
-         if salida.lower() == "s":
-             break
-         elif salida.lower() == "n":
-             return
-         else:
+            salida = input("\n¿Desea ordenar paises por rango de superficie nuevamente?\n - Si desea continuar presione ¨s¨\n - Si desea salir presione ¨n¨\n\n").lower()
+            if salida == "s":
+                break
+            elif salida == "n":
+                return
+            else:
                 print("Error. Ingrese:\n - 's' para continuar\n - 'n' para salir")
--- Gestion de Paises/Ordenar.py
+++ Gestion de Paises/Ordenar.py
@@ -1,11 +1,149 @@
+#ORDEN POR NOMBRE
 def por_nombre(paises):
-    pass
+    while True:
+        #Se ingresa a la función eligiendo la opción deseada
+        print("-----------------------------------------------------")    
+        print("Ingrese la opción deseada: ")
+        opcion = input(" (a) Orden alfabético (A - Z)\n (b) Orden alfabético inverso (Z - A)\n (c) Volver \n\n").upper()
 
+        #Valido que la opción elegida sea correcta
+        while opcion not in ["A", "B", "C"]:
+            print("-----------------------------------------------------")
+            opcion = input("Error. Seleccione una opción válida: \n (a) A - Z\n (b) Z - A\n (c) Volver \n\n").upper()
+
+
+        #Creo el diccionario agrupados: inicial → clave | nombres de países → valor
+        agrupados = {}
+        for pais in paises:
+            inicial = pais[0].upper()
+            if inicial not in agrupados:
+                agrupados[inicial] = []
+            agrupados[inicial].append(pais)
+
+        #Opción A: Orden alfabético
+        if opcion == "A":
+            print("-----------------------------------------------------")
+            print("Orden A - Z:")
+            #Con la función sorted ordeno alfabéticamente las claves del diccionario agrupados{}
+            for letra in sorted(agrupados.keys()):
+                #Uso la función join() para unir todos los elementos del diccionario usando coma y espacio como separador
+                print(f"- {letra}: {', '.join(sorted(agrupados[letra]))}")
+
+        #Opción B: Orden alfabético inverso
+        elif opcion == "B":
+            print("-----------------------------------------------------")
+            print("Orden Z - A:")
+            #Ordeno alfabéticamente todas las claves del diccionario agrupados{} y las muestro en reversa aplicando "reverse=True"
+            for letra in sorted(agrupados.keys(), reverse=True):
+                print(f"- {letra}: {', '.join(sorted(agrupados[letra], reverse=True))}")
+
+        #En caso de que la opción sea "C" el bucle se rompe y vuelve al submenú "Ordenar por:"
+        elif opcion == "C":
+            break
+
+        #Validación de salida
+        salida = input("\n¿Desea ordenar países por orden alfabético nuevamente?\n - Si desea continuar ingrese ¨s¨\n - Si desea salir ingrese ¨n¨\n\n").lower()
+        if salida == "n":
+            break
+        elif salida != "s":
+            print("Error. Ingrese:\n - 's' para continuar\n - 'n' para salir")
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#ORDEN POR POBLACIÓN
 def por_poblacion(paises):
-    pass
+    while True:
+        #Se ingresa a la función eligiendo la opción deseada
+        print("-----------------------------------------------------")
+        print("Ingrese la opción deseada: ")
+        opcion = input(" (a) Población ascendente\n (b) Población descendente\n (c) Top 3 paises más poblados \n (d) Volver\n\n").upper()
 
+        #Valido que la opción elegida sea correcta
+        while opcion not in ["A", "B", "C", "D"]:
+            print("-----------------------------------------------------")
+            opcion = input("\nError. Seleccione una opción válida: \n (a) Población ascendente\n (b) Población descendente\n (c) Top 3 paises más poblados \n (d) Volver\n\n").upper()
+        
+        #Creo función para poder reutilizar según el parámetro de busqueda
+        def mostrar_paises_por_poblacion(paises, descendente=False):
+            lista = [] #Creo la lista para guardar los datos de población
+            for nombre, datos in paises.items():
+                poblacion = datos[1]
+                lista.append([nombre, poblacion]) #Relleno la lista previamente creada
+
+            #key=lambda --> utilizada para crea una función pequeña, de manera ágil. En este caso para ordenar la población
+            lista = sorted(lista, key=lambda x: x[1], reverse=descendente)
+            for nombre, poblacion in lista: #Imprimo los datos de población para cada país
+                print(f"- {nombre}: {poblacion:,} habitantes") 
+        
+        #Población ascendente
+        if opcion == "A":
+            print("-----------------------------------------------------")
+            print("\nPaíses ordenados por población ascendente:")
+            mostrar_paises_por_poblacion(paises) #Llamo a la función solo con el parámetro paises, para que se imprima la población de forma ascendente 
+
+        #Población descendente
+        elif opcion == "B":
+            print("-----------------------------------------------------")
+            print("\nPaíses ordenados por población descendente:")
+            mostrar_paises_por_poblacion(paises, descendente=True) #Agrego el parámetro "descendente=True" para mostrar la población de forma descendente
+            
+
+        #Top 3 paises más poblados
+        elif opcion == "C":
+            top_paises = [] #Creo la lista para guardar los países con mayor población
+            for nombre, datos in paises.items():
+                poblacion = datos[1] #Le asigno los datos de población a la variable población
+                top_paises.append([nombre, poblacion]) #Relleno la lista previamente creada
+
+            #Utilizo nuevamente "key=lambda", en esta ocasión para ordenar la lista de forma descendente (aplicando "reverse=True")
+            top_paises = sorted(top_paises, key=lambda x: x[1], reverse=True)
+
+            print("-----------------------------------------------------")
+            print("\nTop 3 países más poblados:")
+            #Muestro los 3 primeros países de la lista, e decir, los más poblados
+            for nombre, poblacion in top_paises[:3]:
+             print(f"- {nombre}: {poblacion:,} habitantes")
+        
+        #En caso de que la opción sea "D" el bucle se rompe y vuelve al submenú "Ordenar por:"
+        elif opcion == "D":
+            break
+
+        #Validación de salida
+        while True:
+            salida = input("\n¿Desea ordenar paises según su población nuevamente?\n - Si desea continuar presione ¨s¨\n - Si desea salir presione ¨n¨\n\n").lower()
+            if salida == "s":
+                break
+            elif salida == "n":
+                return
+            else:
+                print("Error. Ingrese:\n - 's' para continuar\n - 'n' para salir")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Creo función para modularizar el código y evitar redundancias
+def ordenar_paises_superficie(paises, descendente=False):
+    ordenar_paises = [] #Creo la lista para rellenar los países según su superficie
+    for nombre, datos in paises.items():
+        superficie = datos[2] #Le asigno los datos de superficie a la variable superficie
+        ordenar_paises.append([nombre, superficie]) #Relleno la lista previamente creada
+
+    #Utilizo nuevamente "key=lambda", en esta ocasión para ordenar la lista según necesidad o búsqueda
+    ordenar_paises_superficie = sorted(ordenar_paises, key=lambda x: x[1], reverse=descendente)
+
+    #Llamo a la función según la opción ingresada
+    if descendente:
+        orden = "descendente"
+    else:
+        orden = "ascendente"
+    print("-----------------------------------------------------")
+    print(f"\nPaíses ordenados por superficie {orden}:")
+    for nombre, superficie in ordenar_paises_superficie:
+        print(f"{nombre}: {superficie:,} km²")
+
+
+#ORDEN POR SUPERFICIE ASCENDENTE
 def por_superficie_ascendente(paises):
-    pass
+    ordenar_paises_superficie(paises)
 
+#ORDEN POR SUPERFICIE DESCENDENTE
 def por_superficie_descendente(paises):
-    pass
+    ordenar_paises_superficie(paises, descendente= True)
